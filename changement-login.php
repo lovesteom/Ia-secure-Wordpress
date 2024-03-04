c0fda24c0662c86d5bb0f2bb77faf5cb9690ddc4
Ouéziza <31071183+lovesteom@users.noreply.github.com>
2024-03-04 17:02:15 +0100

    test

--- changement-login.php
+++ changement-login.php
@@ -1,16 +1,169 @@
 <?php
+
+// Ajouter une action pour rediriger les liens "wp-admin"
+add_action( 'init', 'mon_plugin_rediriger_wp_admin' );
+
 // Code spécifique à la fonctionnalité de changement d'URL de connexion
 function afficher_changement_login() {
     ?>
     <div class="wrap">
         <h2>Changement d'URL de Connexion</h2>
 
-        <form method="post" action="">
-            <?php wp_nonce_field('changement_login', 'changement_login_nonce'); ?>
-            <label for="nouvelle_url_login">Nouvelle URL de Connexion :</label>
-            <input type="text" name="nouvelle_url_login" id="nouvelle_url_login" required />
-            <input type="submit" name="submit_nouvelle_url" class="button-primary" value="Enregistrer" />
-        </form>
+       
+<form class="form" id="searchForm" onsubmit="return validateForm()">
+    
+    <p class="message">Veuillez saisir le slug.  </p>
+       
+    <label>
+        <input required="" placeholder="" type="text" class="input" id="searchText">
+        
+    </label> 
+    
+    <button class="submit">Valider</button>
+    
+</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Vérifier si le champ de texte a été soumis et n'est pas vide
+    if (isset($_POST["searchText"]) && !empty($_POST["searchText"])) {
+        // Récupérer la valeur du champ de texte
+        $slugs_change = $_POST["searchText"];
+        
+        function chargeur();
+        
+    } else {
+        // Si le champ de texte est vide, afficher un message d'erreur
+        echo "Veuillez saisir un slug.";
+    }
+}
+?>
+
+<style>
+                .form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            max-width: 350px;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 20px;
+            position: relative;
+            }
+
+            .title {
+            font-size: 28px;
+            color: royalblue;
+            font-weight: 600;
+            letter-spacing: -1px;
+            position: relative;
+            display: flex;
+            align-items: center;
+            padding-left: 30px;
+            }
+
+            .title::before,.title::after {
+            position: absolute;
+            content: "";
+            height: 16px;
+            width: 16px;
+            border-radius: 50%;
+            left: 0px;
+            background-color: royalblue;
+            }
+
+            .title::before {
+            width: 18px;
+            height: 18px;
+            background-color: royalblue;
+            }
+
+            .title::after {
+            width: 18px;
+            height: 18px;
+            animation: pulse 1s linear infinite;
+            }
+
+            .message, .signin {
+            color: rgba(88, 87, 87, 0.822);
+            font-size: 14px;
+            }
+
+            .signin {
+            text-align: center;
+            }
+
+            .signin a {
+            color: royalblue;
+            }
+
+            .signin a:hover {
+            text-decoration: underline royalblue;
+            }
+
+            .form label {
+            position: relative;
+            }
+
+            .form label .input {
+            width: 100%;
+            padding: 10px 10px 20px 10px;
+            outline: 0;
+            border: 1px solid rgba(105, 105, 105, 0.397);
+            border-radius: 10px;
+            }
+
+            .form label .input + span {
+            position: absolute;
+            left: 10px;
+            top: 15px;
+            color: grey;
+            font-size: 0.9em;
+            cursor: text;
+            transition: 0.3s ease;
+            }
+
+            .form label .input:placeholder-shown + span {
+            top: 15px;
+            font-size: 0.9em;
+            }
+
+            .form label .input:focus + span,.form label .input:valid + span {
+            top: 30px;
+            font-size: 0.7em;
+            font-weight: 600;
+            }
+
+            .form label .input:valid + span {
+            color: green;
+            }
+
+            .submit {
+            border: none;
+            outline: none;
+            background-color: royalblue !important;
+            padding: 10px;
+            border-radius: 10px;
+            color: #fff;
+            font-size: 16px;
+            transform: .3s ease;
+            }
+
+            .submit:hover {
+            background-color: rgb(56, 90, 194);
+            }
+
+            @keyframes pulse {
+            from {
+                transform: scale(0.9);
+                opacity: 1;
+            }
+
+            to {
+                transform: scale(1.8);
+                opacity: 0;
+            }
+            }
+</style>
 <?php
     }
 // Globale : Fonction de redirection de redirection de tous url wp-admin vers la page 404 
@@ -38,54 +191,54 @@ function mon_plugin_rediriger_wp_admin() {
   }
 
 }
-$slugs_change='om';
-$slugs_valide=$slugs_change.'.php';
-// Ajouter une action pour rediriger les liens "wp-admin"
-add_action( 'init', 'mon_plugin_rediriger_wp_admin' );
+
 
 
 //Sur cette partie, faire en sorte que quand le changement est fait une fois de plus le faire ou de faire une vérification de fichier << si wp-login.php existe renomé le ficher
-rename("C:/xampp/htdocs/tera/wp-login.php", "C:/xampp/htdocs/tera/$slugs_valide");
+function chargeur()  {  
 
+    rename("C:/xampp/htdocs/tera/wp-login.php", "C:/xampp/htdocs/tera/$slugs_valide");
 
-$chemin_fichier= "C:/xampp/htdocs/tera/$slugs_valide";
-$mot_a_remplacer="wp-login.php";
-$nouveau_mot=$slugs_valide;
-function remplacer_mot_dans_fichier($chemin_fichier, $mot_a_remplacer, $nouveau_mot) {
-  // Ouvrir le fichier en mode lecture
-  if (!$handle = fopen($chemin_fichier, 'r')) {
-      return 'Impossible d\'ouvrir le fichier';
-  }
+    $chemin_fichier= "C:/xampp/htdocs/tera/$slugs_valide";
+    $mot_a_remplacer="wp-login.php";
+    $nouveau_mot=$slugs_valide;
 
-  // Lire le contenu du fichier
-  $contenu_fichier = fread($handle, filesize($chemin_fichier));
-
-  // Fermer le fichier
-  fclose($handle);
-
-  // Remplacer chaque occurrence du mot à remplacer par le nouveau mot
-  $contenu_fichier_modifie = str_replace($mot_a_remplacer, $nouveau_mot, $contenu_fichier);
-
-  // Ouvrir le fichier en mode écriture
-  if (!$handle = fopen($chemin_fichier, 'w')) {
-      return 'Impossible d\'ouvrir le fichier';
-  }
-
-  // Écrire le contenu modifié dans le fichier
-  if (fwrite($handle, $contenu_fichier_modifie) === FALSE) {
-      return 'Impossible d\'écrire dans le fichier';
-  }
-
-  // Fermer le fichier
-  fclose($handle);
-
-  return 'Le remplacement a été effectué avec succès.';
+    function remplacer_mot_dans_fichier($chemin_fichier, $mot_a_remplacer, $nouveau_mot) {
+        // Ouvrir le fichier en mode lecture
+        if (!$handle = fopen($chemin_fichier, 'r')) {
+            return 'Impossible d\'ouvrir le fichier';
+        }
+      
+        // Lire le contenu du fichier
+        $contenu_fichier = fread($handle, filesize($chemin_fichier));
+      
+        // Fermer le fichier
+        fclose($handle);
+      
+        // Remplacer chaque occurrence du mot à remplacer par le nouveau mot
+        $contenu_fichier_modifie = str_replace($mot_a_remplacer, $nouveau_mot, $contenu_fichier);
+      
+        // Ouvrir le fichier en mode écriture
+        if (!$handle = fopen($chemin_fichier, 'w')) {
+            return 'Impossible d\'ouvrir le fichier';
+        }
+      
+        // Écrire le contenu modifié dans le fichier
+        if (fwrite($handle, $contenu_fichier_modifie) === FALSE) {
+            return 'Impossible d\'écrire dans le fichier';
+        }
+      
+        // Fermer le fichier
+        fclose($handle);
+      
+        return 'Le remplacement a été effectué avec succès.';
+      }
+      
+      
+      $resultat = remplacer_mot_dans_fichier($chemin_fichier, $mot_a_remplacer, $nouveau_mot);
+      echo $resultat;
 }
 
-// Utilisation de la fonction avec le chemin vers le fichier et les mots à remplacer
-$chemin_fichier = 'chemin_vers_votre_fichier.txt';
-$mot_a_remplacer = 'ZZZ';
-$nouveau_mot = 'AAA';
 
-$resultat = remplacer_mot_dans_fichier($chemin_fichier, $mot_a_remplacer, $nouveau_mot);
-echo $resultat;
+
+
